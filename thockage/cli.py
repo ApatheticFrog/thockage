8684059065097a46395257285d1fde5675721c81
= <doublenegative@protonmail.com>
2021-08-30 22:57:31 +0200

    cleanup

--- thockage/cli.py
+++ thockage/cli.py
@@ -14,11 +14,8 @@ def manager():
 @click.option("--mode", "-m", type=click.Choice(list(modes.available_modes.keys()), case_sensitive=False))
 def run(mode):
     if mode:
-
-        print(mode)
         modes.change_mode(mode)
         audio.sound_mode_update()
-        print(modes.selected_mode)
 
     # Collect all event until released
     with Listener(on_press = keyboard_control) as listener:   
